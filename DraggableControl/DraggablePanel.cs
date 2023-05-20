816af50e1e1d78787ca86a26511e32ac4e19946c
Kulpot <kulpotwahu@gmail.com>
2023-05-21 06:14:56 +0800

    Add project files.

---
+++ DraggableControl/DraggablePanel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ComponentModel;
+
+namespace DraggableControls
+{
+    class DraggableControl : Control
+    {
+        private Point _lastPos;
+        private bool _movable = true;
+
+        public DraggableControl()
+        {
+            this.Cursor = Cursors.Hand;
+            this.BackColor = Color.CornflowerBlue;
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+
+            if(_movable)
+            {
+                _lastPos = e.Location;
+                this.Cursor = Cursors.SizeAll;
+            }
+        }
+
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            base.OnMouseUp(e);
+
+            if(_movable )
+            {
+                this.Cursor = Cursors.Hand;
+            }
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            
+            if(_movable && e.Button == MouseButtons.Left)
+            {
+                this.Left += e.Location.X - _lastPos.X;
+                this.Top += e.Location.Y - _lastPos.Y;
+            }
+        }
+
+        [DefaultValue(true)]
+        [Description("Determines whether or not the control can be moved by the user")]
+        public bool Movable
+        {
+            get { return _movable; }
+            set
+            {
+                _movable = value;
+
+                if( _movable )
+                {
+                    this.Cursor = Cursors.Hand;
+                }
+                else
+                {
+                    this.Cursor = Cursors.Default;
+                }
+            }
+        }
+    }
+}
---
+++ DraggableControl/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace DraggableControls
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.draggableControl1 = new DraggableControls.DraggableControl();
+            this.SuspendLayout();
+            // 
+            // draggableControl1
+            // 
+            this.draggableControl1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.draggableControl1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.draggableControl1.Location = new System.Drawing.Point(24, 22);
+            this.draggableControl1.Name = "draggableControl1";
+            this.draggableControl1.Size = new System.Drawing.Size(137, 95);
+            this.draggableControl1.TabIndex = 0;
+            this.draggableControl1.Text = "draggableControl1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(352, 205);
+            this.Controls.Add(this.draggableControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DraggableControl draggableControl1;
+    }
+}
+
